89443fe4dcebc75ddd4dcad8806519742188e4de
Ek Chotechawanwong <ekc@acm.org>
2021-11-29 15:04:02 +0700

    Allow echo-server to listen on multiple ports
    (also loop forever)

--- echo-server.py
+++ echo-server.py
@@ -1,15 +1,34 @@
 # Echo server program
-import socket, sys
-
+import socket, sys, thread, time
 HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = int(sys.argv[1])              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print 'Connected by', addr
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+
+def on_new_thread(s):
+    while True:
+        conn, addr = s.accept()
+        data = conn.recv(1024)
+        print conn.getsockname(), addr, ":", data
+        conn.sendall(data)
+        conn.close()
+
+try:
+    ports = [int(port) for port in sys.argv[1:] if (int(port) > 0 and int(port) < 65536)]
+except ValueError as e:
+    print e
+    sys.exit(1)
+ss = []
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "Listening on ", port
+    s.bind((HOST, port))
+    s.listen(5)
+    ss.append(s)
+
+for s in ss:
+    thread.start_new_thread(on_new_thread,(s,))
+
+while True:
+    try:
+        time.sleep(5)
+    except KeyboardInterrupt as e:
+        print "Receive Keyboard Interrupt: exiting ..."
+        sys.exit(0)
